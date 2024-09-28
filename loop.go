776560d0749bd28d5b378f1f74e86a7656659cf6
tanvir35web <islammdtanvirul@gmail.com>
2024-09-29 00:04:25 +0600

    loop related more example  added

--- loop.go
+++ loop.go
@@ -3,30 +3,58 @@ package main
 import "fmt"
 
 func main() {
+	// for loop practices
 
-	i := 1
-	for i <= 3 {
-		fmt.Println("i = ", i)
-		i = i + 1
+	// Example 1: Print numbers from 1 to 10
+	for i := 1; i <= 10; i++ {
+		fmt.Println("increment = ", i)
 	}
 
-	for j := 0; j < 3; j++ {
-		fmt.Println("J = ", j)
+	// Example 2: Print numbers from 10 to 1 in reverse order
+	for i := 10; i >= 1; i-- {
+		fmt.Println("decrement = ", i)
 	}
 
-	for i := range 3 {
-		fmt.Println("range", i)
+	// Example 3: Print even numbers from 0 to 20
+	i := 0
+	for i <= 20 {
+		fmt.Println("Even numbers = ", i)
+		i += 2
 	}
 
-	for {
-		fmt.Println("loop")
-		break
-	}
-
-	for n := range 6 {
-		if n%2 == 0 {
-			continue
+	// Example 4: Print odd numbers from 1 to 20
+	for i := range 20 {
+		if i%2 != 0 {
+			fmt.Println("Odd numbers = ", i+1)
 		}
-		fmt.Println("n = ", n)
 	}
+
+	// Example 5: Print numbers from 1 to 10, but only if they are prime numbers
+	for i := 2; i <= 10; i++ {
+		isPrime := true
+		for j := 2; j < i; j++ {
+			if i%j == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			fmt.Println("Prime numbers = ", i)
+		}
+	}
+
+	// Example 6: Print numbers from 1 to 10, but only if they are perfect squares
+	for i := 1; i <= 10; i++ {
+		isPerfectSquare := false
+		for j := 1; j*j <= i; j++ {
+			if j*j == i {
+				isPerfectSquare = true
+				break
+			}
+		}
+		if isPerfectSquare {
+			fmt.Println("Perfect squares = ", i)
+		}
+	}
+
 }
