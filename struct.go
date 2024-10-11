0c695c0a30e5e0949be1911ba774c685cc7a4eea
Tanvirul Islam <tanvirulislam@Tanviruls-MacBook-Air.local>
2024-10-11 22:17:07 +0600

    new example added using struct

--- struct.go
+++ struct.go
@@ -3,33 +3,22 @@ package main
 import "fmt"
 
 // Defining a struct
-type Student struct {
-	Name  string
-	Age   int
-	Grade int
+type Person struct {
+	name   string
+	age    int
+	gender string
 }
 
-func (s Student) getStudentDetails() {
-	fmt.Printf("Name: %s, Age: %d, Grade: %d\n", s.Name, s.Age, s.Grade)
+func (p Person) Greet() {
+	fmt.Printf("Hello, I am %s. \n Currently I am %d years old \n", p.name, p.age)
 }
 
 func main() {
-	println("Struct in Go Lang which is equivalent to Objects")
-
-	// Creating a new student object
-	student := Student{
-		Name:  "John Doe",
-		Age:   18,
-		Grade: 12,
+	// Creating a new instance of the Person struct
+	person := Person{
+		name:   "Tanvirul Islam",
+		age:    25,
+		gender: "male",
 	}
-
-	// Calling the getStudentDetails method
-	student.getStudentDetails()
-
-	// Changing the student's grade
-	student.Grade = 18
-
-	// Calling the getStudentDetails method again to see the updated grade
-	student.getStudentDetails()
-
+	person.Greet()
 }
